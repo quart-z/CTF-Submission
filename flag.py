f65efa9e61938527fb6f4cbcbfc5a473ac8ae372
quart-z <jtw2212@gmail.com>
2020-11-04 17:53:12 -0700

    Add files via upload

--- flag.py
+++ flag.py
@@ -10,18 +10,16 @@ t = Team("http://actf0-thu.cse365.io/", flagToken) # sets team
 print("Welcome to flag submission! \n")
 
 def submitFlag():
-	choice = 1 # don't change this 
-	while (choice != 0):
+	choice = "1" # don't change this 
+	while (choice != "0"):
 		choice = input("Input flag to submit, or 0 to quit: ") # Takes input as string
-		if (choice == "0"): # If user entered 0 as a string, casts to int to break while loop
-			choice = int(choice)
-		else:
+		if (choice != "0"):
 			try:
 				t.submit_flag([choice]) # Submits string of flag
 				print("Flag successfully submitted, status: ")
 			except:
 				print("Game not running, try again later")
-				choice = 0
+				choice = "0"
 
 	print("Quitting program...") # Only output when while loop breaks
 
